0e4d5632a71c8f9842c639c730bfc70ee07efa6a
bruno.cruz <bruno.cruz@corp.globo.com>
2020-10-17 23:17:33 -0300

    + cria estrutura inicial do projeto

---
+++ app/src/main/java/com/doubleb/meusemestre/Application.kt
@@ -0,0 +1,9 @@
+package com.doubleb.meusemestre
+
+import android.app.Application
+
+class Application : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/doubleb/meusemestre/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.doubleb.meusemestre
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : AppCompatActivity(R.layout.activity_home) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
