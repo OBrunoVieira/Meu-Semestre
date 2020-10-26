d7ba5d317bf410a862a86e6bc37b7a00cbc5e908
bruno.cruz <bruno.cruz@corp.globo.com>
2020-10-26 10:58:21 -0300

    + cria BaseActivity para extrair comportamentos em comum entre Activities

--- app/src/main/java/com/doubleb/meusemestre/ui/activities/HomeActivity.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/activities/HomeActivity.kt
@@ -1,37 +1,19 @@
 package com.doubleb.meusemestre.ui.activities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.ConcatAdapter
+import androidx.fragment.app.Fragment
 import com.doubleb.meusemestre.R
-import com.doubleb.meusemestre.models.Discipline
-import com.doubleb.meusemestre.ui.adapters.BestDisciplineAdapter
-import com.doubleb.meusemestre.ui.adapters.DisciplineListAdapter
-import kotlinx.android.synthetic.main.activity_home.*
+import com.doubleb.meusemestre.ui.fragments.DashboardFragment
 
-class HomeActivity : AppCompatActivity(R.layout.activity_home) {
-
-    private val disciplineListAdapter by lazy { DisciplineListAdapter() }
-    private val bestDisciplineAdapter by lazy { BestDisciplineAdapter() }
-    private val concatAdapter by lazy { ConcatAdapter(disciplineListAdapter, bestDisciplineAdapter) }
+class HomeActivity : BaseActivity(R.layout.activity_home) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        dashboard_recycler_view.adapter = concatAdapter
-        bestDisciplineAdapter.discipline = Discipline("", "Fotografia", 10f)
+        inflateFragment(DashboardFragment())
+    }
 
-
-        disciplineListAdapter.list = listOf(
-            Discipline("", "Fotografia", 10f),
-            Discipline("", "Sociologia", 2f),
-            Discipline("", "Desenvolvimento de Banco de Dados II", 2f),
-            Discipline("", "Química", 5f),
-            Discipline("", "Álgebra Linear", 3.4f),
-            Discipline("", "Cálculo I", 7f),
-            Discipline("", "Ilustração", 9f),
-            Discipline("", "Literatura Moderna", 9.5f),
-            Discipline("", "Literatura Clássica", 8f)
-        )
+    private fun inflateFragment(fragment: Fragment) {
+        inflateFragment(R.id.home_fragment_container, fragment)
     }
 
 }
