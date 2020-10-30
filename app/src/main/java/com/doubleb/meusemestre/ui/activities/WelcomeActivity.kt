36f422a09151b12457a19baccc76365489dbffc8
bruno.cruz <bruno.cruz@corp.globo.com>
2020-10-29 22:11:09 -0300

    + cria estrutura inicial do fluxo principal de `Welcome`

---
+++ app/src/main/java/com/doubleb/meusemestre/ui/activities/WelcomeActivity.kt
@@ -0,0 +1,57 @@
+package com.doubleb.meusemestre.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.doubleb.meusemestre.R
+import com.doubleb.meusemestre.extensions.blendColorAnimation
+import com.doubleb.meusemestre.ui.adapters.WelcomeAdapter
+import kotlinx.android.synthetic.main.activity_welcome.*
+
+class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
+    private var previousPosition = 0
+
+    private val colors by lazy {
+        arrayOf(
+            R.color.dark_purple,
+            R.color.dark_blue,
+            R.color.dark_yellow
+        )
+    }
+
+    private val pageChangeCallback by lazy { onPageChangeCallback() }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        welcome_view_pager.apply {
+            adapter = WelcomeAdapter(this@WelcomeActivity)
+            welcome_page_indicator.count = (adapter as WelcomeAdapter).itemCount
+            registerOnPageChangeCallback(pageChangeCallback)
+        }
+
+        welcome_button_explore.setOnClickListener {
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        welcome_view_pager.unregisterOnPageChangeCallback(pageChangeCallback)
+    }
+
+    private fun onPageChangeCallback() = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            welcome_page_indicator.selection = position
+
+            welcome_content_main.blendColorAnimation(
+                colors[previousPosition % colors.size],
+                colors[position % colors.size],
+                "backgroundColor"
+            )
+
+            previousPosition = position
+        }
+    }
+}
---
+++ app/src/main/java/com/doubleb/meusemestre/ui/adapters/WelcomeAdapter.kt
@@ -0,0 +1,40 @@
+package com.doubleb.meusemestre.ui.adapters
+
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.doubleb.meusemestre.R
+import com.doubleb.meusemestre.ui.fragments.WelcomeInfoFragment
+
+class WelcomeAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount() = 4
+    override fun createFragment(position: Int) =
+        when (position) {
+            0 ->
+                WelcomeInfoFragment.newInstance(
+                    R.drawable.shape_profile,
+                    R.string.welcome_info_first_title,
+                    R.string.welcome_info_first_description
+                )
+
+            1 ->
+                WelcomeInfoFragment.newInstance(
+                    R.drawable.shape_profile,
+                    R.string.welcome_info_second_title,
+                    R.string.welcome_info_second_description
+                )
+
+            2 ->
+                WelcomeInfoFragment.newInstance(
+                    R.drawable.shape_profile,
+                    R.string.welcome_info_third_title,
+                    R.string.welcome_info_third_description
+                )
+
+            else ->
+                WelcomeInfoFragment.newInstance(
+                    R.drawable.shape_profile,
+                    R.string.welcome_info_fourth_title,
+                    R.string.welcome_info_fourth_description
+                )
+        }
+}
