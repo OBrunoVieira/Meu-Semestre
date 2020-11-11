44e2ce761a24d88f04f39ba4ccda4739f252f02d
Bruno Vieira <brunovieiracrz@gmail.com>
2020-11-10 23:55:39 -0300

    + adiciona estrutura inicial de histórico de discplinas

---
+++ app/src/main/java/com/doubleb/meusemestre/ui/fragments/SemesterHistoryFragment.kt
@@ -0,0 +1,6 @@
+package com.doubleb.meusemestre.ui.fragments
+
+import androidx.fragment.app.Fragment
+import com.doubleb.meusemestre.R
+
+class SemesterHistoryFragment : Fragment(R.layout.fragment_semester_history)
