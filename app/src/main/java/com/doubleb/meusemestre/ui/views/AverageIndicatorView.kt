66524d91072c33406fc5d27a48a3badd8779c07a
Bruno Vieira <brunovieiracrz@gmail.com>
2020-11-17 22:09:21 -0300

    + ajusta componente AverageIndicatorView para tratar título também

--- app/src/main/java/com/doubleb/meusemestre/ui/views/AverageIndicatorView.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/views/AverageIndicatorView.kt
@@ -2,6 +2,7 @@ package com.doubleb.meusemestre.ui.views
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.View
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.text.HtmlCompat
@@ -15,18 +16,50 @@ class AverageIndicatorView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
+    private val titleTextSize: Int
+    private val titleMinLines: Int
+    private val titleSpacing: Int
+
     init {
         View.inflate(context, R.layout.view_average_indicator, this)
+
+        val typedArray =
+            context.obtainStyledAttributes(attrs, R.styleable.AverageIndicatorView, defStyleAttr, 0)
+        titleTextSize = typedArray.getDimensionPixelSize(
+            R.styleable.AverageIndicatorView_aiv_title_height,
+            resources.getDimensionPixelSize(R.dimen.text_size_sixteen)
+        )
+
+        titleMinLines = typedArray.getInteger(
+            R.styleable.AverageIndicatorView_aiv_title_min_lines,
+            2
+        )
+
+        titleSpacing = typedArray.getDimensionPixelSize(
+            R.styleable.AverageIndicatorView_aiv_title_spacing,
+            resources.getDimensionPixelSize(R.dimen.spacing_eight)
+        )
+
+        typedArray.recycle()
+
         minWidth = resources.getDimensionPixelSize(R.dimen.max_discipline_width)
     }
 
     override fun onFinishInflate() {
         super.onFinishInflate()
+        discipline_text_view_title.setTextSize(TypedValue.COMPLEX_UNIT_PX, titleTextSize.toFloat())
+        (discipline_text_view_title.layoutParams as MarginLayoutParams).bottomMargin = titleSpacing
+        discipline_text_view_title.minLines = titleMinLines
+
         average(null)
     }
 
-    fun average(average: Float?) =
-        average?.let {
+    fun title(title: String?) = apply {
+        discipline_text_view_title.text = title
+    }
+
+    fun average(average: Float?) = apply {
+        average?.takeIf { it >= 0 }?.let {
             average_indicator_text_view_average_title.text = transformGradeToText(average)
 
             average_indicator_progress.progress = ((average / 10) * 100).toInt()
@@ -35,6 +68,7 @@ class AverageIndicatorView @JvmOverloads constructor(
             average_indicator_text_view_average_title.setText(R.string.average_indicator_empty_average)
             average_indicator_progress.gone()
         }
+    }
 
     private fun transformGradeToText(grade: Float) =
         HtmlCompat.fromHtml(
