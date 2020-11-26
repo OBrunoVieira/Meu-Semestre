036a4081eb6ad0643b95f7316c05dda916290a5b
Bruno Vieira <brunovieiracrz@gmail.com>
2020-11-26 20:44:34 -0300

    + adiciona estrutura inicial de login

--- app/src/main/java/com/doubleb/meusemestre/ui/activities/WelcomeActivity.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/activities/WelcomeActivity.kt
@@ -5,10 +5,20 @@ import android.os.Bundle
 import androidx.viewpager2.widget.ViewPager2
 import com.doubleb.meusemestre.R
 import com.doubleb.meusemestre.extensions.blendColorAnimation
+import com.doubleb.meusemestre.models.User
 import com.doubleb.meusemestre.ui.adapters.viewpager.WelcomePageAdapter
+import com.doubleb.meusemestre.ui.dialogs.BottomSheetLogin
+import com.facebook.*
+import com.facebook.login.LoginManager
+import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_welcome.*
 
-class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
+
+class WelcomeActivity : BaseActivity(R.layout.activity_welcome), FacebookCallback<LoginResult>,
+    BottomSheetLogin.LoginClickListener {
     private var previousPosition = 0
 
     private val colors by lazy {
@@ -20,9 +30,16 @@ class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
     }
 
     private val pageChangeCallback by lazy { onPageChangeCallback() }
+    private val bottomSheet by lazy { BottomSheetLogin(this) }
+    private val callbackManager by lazy { CallbackManager.Factory.create() }
+
+    private val auth by lazy { FirebaseAuth.getInstance() }
+    private val database by lazy { FirebaseDatabase.getInstance().reference }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        LoginManager.getInstance().registerCallback(callbackManager, this)
+
         welcome_view_pager.apply {
             adapter = WelcomePageAdapter(this@WelcomeActivity)
             welcome_page_indicator.count = (adapter as WelcomePageAdapter).itemCount
@@ -34,12 +51,20 @@ class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
         }
 
         welcome_button_login.setOnClickListener {
-            startActivity(Intent(this, HomeActivity::class.java))
-            finish()
+            bottomSheet.show(supportFragmentManager, BottomSheetLogin.TAG)
         }
     }
 
     override fun onDestroy() {
+        GraphRequest(
+            AccessToken.getCurrentAccessToken(),
+            "/me/permissions/", null, HttpMethod.DELETE
+        ) {
+            AccessToken.setCurrentAccessToken(null)
+            LoginManager.getInstance().logOut()
+            auth.signOut()
+        }.executeAsync()
+
         super.onDestroy()
         welcome_view_pager.unregisterOnPageChangeCallback(pageChangeCallback)
     }
@@ -58,4 +83,72 @@ class WelcomeActivity : BaseActivity(R.layout.activity_welcome) {
             previousPosition = position
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    //region Facebook Login's callback
+    override fun onSuccess(result: LoginResult?) {
+        result?.let {
+            handleFacebookAccessToken(it.accessToken)
+        }
+    }
+
+    override fun onCancel() {
+    }
+
+    override fun onError(error: FacebookException?) {
+    }
+    //endregion
+
+    //region Login Button's callback
+    override fun onFacebookLogin() {
+        LoginManager.getInstance()
+            .logInWithReadPermissions(this, listOf("public_profile", "email"))
+    }
+
+    override fun onGoogleLogin() {
+    }
+
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+
+                    GraphRequest.newMeRequest(
+                        AccessToken.getCurrentAccessToken()
+                    ) { json, response ->
+                        val data = json.getJSONObject("picture").getJSONObject("data")
+                        database.child("users")
+                            .child(token.userId)
+                            .setValue(
+                                User(
+                                    json.getString("name"),
+                                    json.getString("email"),
+                                    data.getString("url")
+                                )
+                            )
+
+                        startActivity(Intent(this, HomeActivity::class.java))
+                        finish()
+                    }.also {
+                        it.parameters =
+                            Bundle().apply {
+                                putString(
+                                    "fields",
+                                    "id,name,email,gender,birthday,picture.type(large)"
+                                )
+                            }
+                    }.executeAsync()
+
+
+                } else {
+                    //failure
+                }
+            }
+    }
+
 }
