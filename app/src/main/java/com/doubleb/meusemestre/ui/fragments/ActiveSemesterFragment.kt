54b8fb1c9f0b1a2ba0a1580166615535a74e07e6
Bruno Vieira <brunovieiracrz@gmail.com>
2020-12-08 23:37:02 -0300

    + migra regra de exibiçao de FAB para o filho responsável, ActiveSemesterFragment

--- app/src/main/java/com/doubleb/meusemestre/ui/fragments/ActiveSemesterFragment.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/fragments/ActiveSemesterFragment.kt
@@ -6,10 +6,11 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ConcatAdapter
 import com.doubleb.meusemestre.R
+import com.doubleb.meusemestre.extensions.gone
 import com.doubleb.meusemestre.ui.activities.DisciplineRegistrationActivity
 import com.doubleb.meusemestre.ui.activities.HomeActivity
 import com.doubleb.meusemestre.ui.adapters.recyclerview.ActiveSemesterAdapter
-import com.doubleb.meusemestre.ui.adapters.recyclerview.EmptySemesterAdapter
+import com.doubleb.meusemestre.ui.adapters.recyclerview.EmptyDisciplinesAdapter
 import com.doubleb.meusemestre.ui.listeners.DisciplineListener
 import com.doubleb.meusemestre.ui.views.EmptyStateView
 import kotlinx.android.synthetic.main.fragment_active_semester.*
@@ -17,20 +18,64 @@ import kotlinx.android.synthetic.main.fragment_active_semester.*
 class ActiveSemesterFragment : Fragment(R.layout.fragment_active_semester), DisciplineListener,
     EmptyStateView.ClickListener {
 
-    private val adapter by lazy { ActiveSemesterAdapter(this) }
-    private val emptyDisciplinesAdapter by lazy { EmptySemesterAdapter(this) }
-    private val concatAdapter by lazy { ConcatAdapter(emptyDisciplinesAdapter) }
+    //region immutable vars
 
+    //region adapters
+    private val activeSemesterAdapter by lazy { ActiveSemesterAdapter(this) }
+    private val emptyDisciplinesAdapter by lazy { EmptyDisciplinesAdapter(this) }
+    private val concatAdapter by lazy { ConcatAdapter(emptyDisciplinesAdapter) }
+    //endregion
+
+    //region components
+    private val fab by lazy { (activity as? HomeActivity)?.fab() }
+    //endregion
+
+    //region listeners
+    private val clickListener by lazy {
+        View.OnClickListener {
+            startActivity(Intent(it.context, DisciplineRegistrationActivity::class.java))
+        }
+    }
+    //endregion
+
+    //endregion
+
+    //region lifecycle
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         active_semester_recycler_view.adapter = concatAdapter
     }
 
+    override fun onResume() {
+        super.onResume()
+        fab?.let {
+            it.setOnClickListener(clickListener)
+            it.setText(R.string.active_semester_fab)
+
+            if (activeSemesterAdapter.currentList.isNotEmpty()) {
+                it.show()
+                it.extend()
+            } else {
+                it.gone()
+            }
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        fab?.let {
+            it.gone()
+            it.setOnClickListener(null)
+        }
+    }
+    //endregion
+
+    //region listeners
     override fun onDisciplineClick(position: Int) {
         (activity as? HomeActivity)?.inflateStackFragment(
             DisciplineDetailsFragment.instance(
-                adapter.currentList[position].name,
-                adapter.currentList[position].grade
+                activeSemesterAdapter.currentList[position].name,
+                activeSemesterAdapter.currentList[position].grade
             )
         )
     }
@@ -38,4 +83,5 @@ class ActiveSemesterFragment : Fragment(R.layout.fragment_active_semester), Disc
     override fun onEmptyViewActionClick(view: View) {
         startActivity(Intent(view.context, DisciplineRegistrationActivity::class.java))
     }
+    //endregion
 }
--- app/src/main/java/com/doubleb/meusemestre/ui/fragments/DisciplinesFragment.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/fragments/DisciplinesFragment.kt
@@ -1,25 +1,14 @@
 package com.doubleb.meusemestre.ui.fragments
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import com.doubleb.meusemestre.R
-import com.doubleb.meusemestre.extensions.gone
-import com.doubleb.meusemestre.ui.activities.DisciplineRegistrationActivity
-import com.doubleb.meusemestre.ui.activities.HomeActivity
 import com.doubleb.meusemestre.ui.adapters.viewpager.DisciplinePageAdapter
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.fragment_disciplines.*
 
-class DisciplinesFragment : Fragment(R.layout.fragment_disciplines),
-    TabLayout.OnTabSelectedListener {
-    private val clickListener by lazy {
-        View.OnClickListener {
-            startActivity(Intent(it.context, DisciplineRegistrationActivity::class.java))
-        }
-    }
+class DisciplinesFragment : Fragment(R.layout.fragment_disciplines) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,44 +17,5 @@ class DisciplinesFragment : Fragment(R.layout.fragment_disciplines),
         TabLayoutMediator(disciplines_tab_layout, disciplines_view_pager) { tab, position ->
             tab.text = resources.getStringArray(R.array.disciplines_tab_item)[position]
         }.attach()
-
-        disciplines_tab_layout.addOnTabSelectedListener(this)
     }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        disciplines_tab_layout.removeOnTabSelectedListener(this)
-    }
-
-    override fun onStart() {
-        super.onStart()
-        (activity as? HomeActivity)?.fab()?.let {
-            it.setOnClickListener(clickListener)
-            it.setText(R.string.active_semester_fab)
-            it.show()
-            it.extend()
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        (activity as? HomeActivity)?.fab()?.let {
-            it.gone()
-            it.setOnClickListener(null)
-        }
-    }
-
-    override fun onTabSelected(tab: TabLayout.Tab?) {
-        (activity as? HomeActivity)?.fab()?.let {
-            if (tab?.position == 0) {
-                it.show()
-            } else {
-                it.gone()
-            }
-        }
-    }
-
-    override fun onTabUnselected(tab: TabLayout.Tab?) {}
-
-    override fun onTabReselected(tab: TabLayout.Tab?) {}
 }
