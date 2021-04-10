88cc7b70040c4691fb1b103a3fe91331855ef1e4
Bruno Vieira <brunovieiracrz@gmail.com>
2021-04-10 18:29:53 -0300

    + adiciona `TipsRepository ao `repositoryModule

--- app/src/main/java/com/doubleb/meusemestre/di/RepositoryModule.kt
+++ app/src/main/java/com/doubleb/meusemestre/di/RepositoryModule.kt
@@ -2,11 +2,15 @@ package com.doubleb.meusemestre.di
 
 import com.doubleb.meusemestre.repository.DisciplinesRepository
 import com.doubleb.meusemestre.repository.SemesterRepository
+import com.doubleb.meusemestre.repository.TipsRepository
 import com.doubleb.meusemestre.repository.UserRepository
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import org.koin.dsl.module
 
+@ExperimentalCoroutinesApi
 val repositoryModule = module {
     single { UserRepository(get()) }
     single { SemesterRepository(get(), get()) }
     single { DisciplinesRepository(get(), get()) }
+    single { TipsRepository(get()) }
 }
