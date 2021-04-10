ac5a3fba63ba495b6a428025251e56cd88b12f27
Bruno Vieira <brunovieiracrz@gmail.com>
2021-04-10 18:29:24 -0300

    + adiciona FirebaseStorage ao `databaseModule`

--- app/src/main/java/com/doubleb/meusemestre/di/DatabaseModule.kt
+++ app/src/main/java/com/doubleb/meusemestre/di/DatabaseModule.kt
@@ -2,9 +2,11 @@ package com.doubleb.meusemestre.di
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.storage.FirebaseStorage
 import org.koin.dsl.module
 
 val databaseModule = module {
     factory { FirebaseDatabase.getInstance().reference }
+    factory { FirebaseStorage.getInstance().reference }
     factory { FirebaseAuth.getInstance() }
 }
