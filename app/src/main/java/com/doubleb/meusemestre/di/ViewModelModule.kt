2fe5fd331e64dde147c691e7295dd4912bfe531b
Bruno Vieira <brunovieiracrz@gmail.com>
2021-04-10 18:30:17 -0300

    + adiciona `TipsViewModel` ao `viewModelModule`

--- app/src/main/java/com/doubleb/meusemestre/di/ViewModelModule.kt
+++ app/src/main/java/com/doubleb/meusemestre/di/ViewModelModule.kt
@@ -3,6 +3,7 @@ package com.doubleb.meusemestre.di
 import com.doubleb.meusemestre.viewmodel.DisciplinesViewModel
 import com.doubleb.meusemestre.viewmodel.LoginViewModel
 import com.doubleb.meusemestre.viewmodel.SemesterViewModel
+import com.doubleb.meusemestre.viewmodel.TipsViewModel
 import org.koin.android.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -10,4 +11,5 @@ val viewModelModule = module {
     viewModel { LoginViewModel(get(), get()) }
     viewModel { SemesterViewModel(get()) }
     viewModel { DisciplinesViewModel(get(), get()) }
+    viewModel { TipsViewModel(get()) }
 }
