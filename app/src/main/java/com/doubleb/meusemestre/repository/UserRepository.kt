1ff5381319b1b4692608ad8160c7344ef81b8528
Bruno Vieira <brunovieiracrz@gmail.com>
2021-04-10 19:14:00 -0300

    + adiciona novas chamadas ao `UserRepository`

--- app/src/main/java/com/doubleb/meusemestre/repository/UserRepository.kt
+++ app/src/main/java/com/doubleb/meusemestre/repository/UserRepository.kt
@@ -1,7 +1,11 @@
 package com.doubleb.meusemestre.repository
 
+import com.doubleb.meusemestre.models.GraduationInfo
 import com.doubleb.meusemestre.models.User
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
 
 class UserRepository(private val database: DatabaseReference) {
 
@@ -9,10 +13,33 @@ class UserRepository(private val database: DatabaseReference) {
         private const val DATABASE_USERS = "users"
     }
 
+    //region immutable vars
+    val databaseUser by lazy { database.child(DATABASE_USERS).apply { keepSynced(true) } }
+    //endregion
+
+    fun getUser(userId: String) = database.child(userId).addValueEventListener(
+        object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+    )
+
     fun createUser(user: User) {
-        database.child(DATABASE_USERS)
+        databaseUser
             .child(user.id.orEmpty())
             .setValue(user)
+    }
+
+    fun createGraduationInfo(userId: String, graduationInfo: GraduationInfo) {
+        databaseUser
+            .child(userId)
+            .setValue(graduationInfo)
     }
 
 }
