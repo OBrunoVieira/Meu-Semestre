17f521ac8e32c54b25c2de44f0611dfa3067244d
Bruno Vieira <brunovieiracrz@gmail.com>
2021-05-07 14:21:05 -0300

    + ajusta registro de semestre na BottomSheet

--- app/src/main/java/com/doubleb/meusemestre/ui/dialogs/BottomSheetSemesterRegistration.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/dialogs/BottomSheetSemesterRegistration.kt
@@ -9,12 +9,14 @@ import android.view.ViewGroup
 import com.doubleb.meusemestre.R
 import com.doubleb.meusemestre.extensions.disable
 import com.doubleb.meusemestre.extensions.enable
+import com.doubleb.meusemestre.extensions.invisible
+import com.doubleb.meusemestre.extensions.visible
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_sheet_semester_registration.*
 
 class BottomSheetSemesterRegistration(val listener: SemesterRegistrationClickListener? = null) :
-    BottomSheetDialogFragment(),
-    TextWatcher {
+    BottomSheetDialogFragment(), TextWatcher {
+
     companion object {
         const val TAG = "BottomSheetSemesterRegistration"
     }
@@ -22,17 +24,21 @@ class BottomSheetSemesterRegistration(val listener: SemesterRegistrationClickLis
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? = inflater.inflate(R.layout.bottom_sheet_semester_registration, container, false)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         semester_registration_button.disable()
+        isCancelable = true
 
         semester_registration_edit_text.addTextChangedListener(this)
         semester_registration_button.setOnClickListener {
+            isCancelable = false
             listener?.onCreateSemester(semester_registration_edit_text.text.toString())
-            dismiss()
+            semester_registration_text_input.disable()
+            semester_registration_button.invisible()
+            semester_registration_loading.visible()
         }
     }
 
