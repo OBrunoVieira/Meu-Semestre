1abbf4373474676bf73128a1d21b45505fb0c445
Bruno Vieira <brunovieiracrz@gmail.com>
2021-05-28 00:04:24 -0300

    + ajusta action button no fluxo da bottom sheet de cadastro de semestre

--- app/src/main/java/com/doubleb/meusemestre/ui/dialogs/BottomSheetSemesterRegistration.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/dialogs/BottomSheetSemesterRegistration.kt
@@ -8,9 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import com.doubleb.meusemestre.R
 import com.doubleb.meusemestre.extensions.disable
-import com.doubleb.meusemestre.extensions.enable
-import com.doubleb.meusemestre.extensions.invisible
-import com.doubleb.meusemestre.extensions.visible
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.bottom_sheet_semester_registration.*
 
@@ -33,12 +30,11 @@ class BottomSheetSemesterRegistration(val listener: SemesterRegistrationClickLis
         isCancelable = true
 
         semester_registration_edit_text.addTextChangedListener(this)
-        semester_registration_button.setOnClickListener {
+
+        semester_registration_button.listener {
             isCancelable = false
             listener?.onCreateSemester(semester_registration_edit_text.text.toString())
             semester_registration_text_input.disable()
-            semester_registration_button.invisible()
-            semester_registration_loading.visible()
         }
     }
 
@@ -53,11 +49,7 @@ class BottomSheetSemesterRegistration(val listener: SemesterRegistrationClickLis
 
     override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
         val shouldEnableButton = charSequence != null && charSequence.length > 3
-        if (shouldEnableButton) {
-            semester_registration_button.enable()
-        } else {
-            semester_registration_button.disable()
-        }
+        semester_registration_button.isEnabled(shouldEnableButton)
     }
 
     interface SemesterRegistrationClickListener {
