17dd665a33b6bcb6ed6e5d4c513ed1cce0ed5d6a
Bruno Vieira <brunovieiracrz@gmail.com>
2021-05-28 00:00:38 -0300

    + adiciona tela de splash no produto

---
+++ app/src/main/java/com/doubleb/meusemestre/ui/activities/SplashActivity.kt
@@ -0,0 +1,58 @@
+package com.doubleb.meusemestre.ui.activities
+
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import com.doubleb.meusemestre.R
+import com.doubleb.meusemestre.extensions.openActivity
+import com.doubleb.meusemestre.extensions.openClearedActivity
+import com.doubleb.meusemestre.models.User
+import com.doubleb.meusemestre.viewmodel.DataSource
+import com.doubleb.meusemestre.viewmodel.DataState
+import com.doubleb.meusemestre.viewmodel.LoginViewModel
+import com.doubleb.meusemestre.viewmodel.UserViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
+
+class SplashActivity : BaseActivity(R.layout.activity_splash_screen) {
+    //region viewModels
+    private val loginViewModel: LoginViewModel by inject()
+    private val userViewModel: UserViewModel by inject()
+    //endregion
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        loginViewModel.liveData.observe(this, observeLogin())
+        userViewModel.liveDataUser.observe(this, observeUser())
+
+
+        lifecycleScope.launch(Dispatchers.Main) {
+            delay(1000)
+            loginViewModel.validateCurrentSession()
+        }
+    }
+
+    //region observers
+    private fun observeLogin() = Observer<DataSource<User>> {
+        if (it.dataState == DataState.SUCCESS) {
+            userViewModel.getUser()
+        } else if (it.dataState == DataState.ERROR) {
+            openActivity<WelcomeActivity>()
+        }
+    }
+
+    private fun observeUser() = Observer<DataSource<User>> {
+        if (it.dataState == DataState.SUCCESS) {
+            if (it.data?.graduation_info?.isValid() == true) {
+                openClearedActivity<HomeActivity>()
+            } else {
+                openClearedActivity<RegisterActivity>()
+            }
+        } else if (it.dataState == DataState.ERROR) {
+            openActivity<WelcomeActivity>()
+        }
+    }
+    //endregion
+}
