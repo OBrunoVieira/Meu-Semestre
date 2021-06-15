307f5e31f118726d22787f0e012b8be04be12fce
Bruno Vieira <brunovieiracrz@gmail.com>
2021-06-14 22:53:44 -0300

    + atualiza ilustrações da página de `welcome`

--- app/src/main/java/com/doubleb/meusemestre/ui/adapters/viewpager/WelcomePageAdapter.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/adapters/viewpager/WelcomePageAdapter.kt
@@ -5,34 +5,35 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.doubleb.meusemestre.R
 import com.doubleb.meusemestre.ui.fragments.WelcomeInfoFragment
 
-class WelcomePageAdapter(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
+class WelcomePageAdapter(fragmentActivity: FragmentActivity) :
+    FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount() = 4
     override fun createFragment(position: Int) =
         when (position) {
             0 ->
                 WelcomeInfoFragment.newInstance(
-                    R.drawable.shape_profile,
+                    R.drawable.vector_hello,
                     R.string.welcome_info_first_title,
                     R.string.welcome_info_first_description
                 )
 
             1 ->
                 WelcomeInfoFragment.newInstance(
-                    R.drawable.shape_profile,
+                    R.drawable.vector_updated_grades,
                     R.string.welcome_info_second_title,
                     R.string.welcome_info_second_description
                 )
 
             2 ->
                 WelcomeInfoFragment.newInstance(
-                    R.drawable.shape_profile,
+                    R.drawable.vector_register_grades,
                     R.string.welcome_info_third_title,
                     R.string.welcome_info_third_description
                 )
 
             else ->
                 WelcomeInfoFragment.newInstance(
-                    R.drawable.shape_profile,
+                    R.drawable.vector_history,
                     R.string.welcome_info_fourth_title,
                     R.string.welcome_info_fourth_description
                 )
