6599dc1fb9b287937073fb58b32fc2f857d23de7
Bruno Vieira <brunovieiracrz@gmail.com>
2021-06-14 22:49:00 -0300

    + ajusta animação de `averageIndicatorView`

--- app/src/main/java/com/doubleb/meusemestre/ui/views/AverageIndicatorView.kt
+++ app/src/main/java/com/doubleb/meusemestre/ui/views/AverageIndicatorView.kt
@@ -1,13 +1,15 @@
 package com.doubleb.meusemestre.ui.views
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.util.TypedValue
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.text.HtmlCompat
 import com.doubleb.meusemestre.R
-import com.doubleb.meusemestre.extensions.gone
+import com.doubleb.meusemestre.extensions.invisible
 import com.doubleb.meusemestre.extensions.roundWhenBase10
 import com.doubleb.meusemestre.extensions.takeIfValid
 import com.doubleb.meusemestre.extensions.visible
@@ -17,9 +19,15 @@ class AverageIndicatorView @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0,
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
+    companion object {
+        private const val PROGRESS_DURATION = 600L
+    }
+
     private val titleTextSize: Int
     private val titleMinLines: Int
     private val titleSpacing: Int
+
+    private var previousProgress = 0
 
     init {
         View.inflate(context, R.layout.view_average_indicator, this)
@@ -63,13 +71,26 @@ class AverageIndicatorView @JvmOverloads constructor(
         average?.takeIf { it >= 0 }?.let {
             average_indicator_text_view_average_title.text = transformGradeToText(average)
 
-            average_indicator_progress.progress = ((average / 10) * 100).toInt()
+            val currentProgress = ((average / 10) * 100).toInt()
+            runProgressAnimation(previousProgress, currentProgress)
             average_indicator_progress.visible()
+
+            previousProgress = currentProgress
         } ?: run {
             average_indicator_text_view_average_title.setText(R.string.average_indicator_empty_average)
-            average_indicator_progress.gone()
+            average_indicator_progress.invisible()
+
+            previousProgress = 0
         }
     }
+
+    private fun runProgressAnimation(from: Int, to: Int) =
+        ObjectAnimator.ofInt(average_indicator_progress, "progress", from, to)
+            .apply {
+                interpolator = AccelerateDecelerateInterpolator()
+                duration = PROGRESS_DURATION
+            }
+            .start()
 
     fun recoverContentDescription(name: String?, average: Float?) =
         run {
